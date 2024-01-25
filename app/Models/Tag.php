ef6e6a66f0e807032ffda46d5ae83b1a73a3e4fd
fajri haryanto <151033283+yoshioakio@users.noreply.github.com>
2024-01-25 19:35:13 +0700

    Update Tag.php

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
-{
+class Tag extends Model{
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = [
+        'name'
+    ];    
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
