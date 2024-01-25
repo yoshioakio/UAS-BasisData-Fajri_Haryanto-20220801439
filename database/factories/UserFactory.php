2bc9206de34dc2c147e6d2433e8d05d7b36ec8b1
fajri haryanto <151033283+yoshioakio@users.noreply.github.com>
2024-01-25 19:31:40 +0700

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,6 +15,7 @@ class UserFactory extends Factory
      * The current password being used by the factory.
      */
     protected static ?string $password;
+    
     /**
      * Define the model's default state.
      *
