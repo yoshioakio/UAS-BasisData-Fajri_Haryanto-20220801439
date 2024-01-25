e8546cae26e39f03d1fb4f6757ed29fc7542543f
fajri haryanto <151033283+yoshioakio@users.noreply.github.com>
2024-01-25 19:31:01 +0700

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -10,6 +11,10 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+     /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
     /**
      * Define the model's default state.
      *
